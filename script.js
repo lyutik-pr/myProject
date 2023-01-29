fd528aac8ae8912e9c09db68fea8161b28926e0d
Lyudmila Prylutskaya <lyutik_@mail.ru>
2023-01-30 01:18:44 +0300

    add other tasks

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ alert ("Hello");
 console.log ("Ok");
 
 title = "The first project";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 100;
+fullPrice = 500;
+adaptive = true;
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей"); 
+console.log("Стоимость разработки сайта " + fullPrice + " рублей"); 
+console.log(screens.toLowerCase(), screens.split(", "));
+console.log("Процент отката посреднику за работу " + fullPrice*rollback/100);
