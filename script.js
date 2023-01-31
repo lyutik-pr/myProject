2aed81c2b04638d3d7275f343d554fe27d505059
Lyudmila Prylutskaya <lyutik_@mail.ru>
2023-01-31 21:20:22 +0300

    add tasks lesson03

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+'use strict';
+
 let title = "title";
 let screens = 1;
 let screenPrice = 2;
-let rollback = 3;
+let rollback = 30.1;
 let fullPrice = 11;
 let adaptive = "adaptive";
 alert ("Hello");
@@ -20,3 +22,54 @@ console.log("Стоимость верстки экранов " + screenPrice + 
 console.log("Стоимость разработки сайта " + fullPrice + " рублей"); 
 console.log(screens.toLowerCase(), screens.split(", "));
 console.log("Процент отката посреднику за работу " + fullPrice*rollback/100);
+
+title = prompt("Как называется ваш проект?", "Верстка сайта");
+console.log(title);
+
+screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+console.log(screens);
+
+screenPrice = +prompt("Сколько будет стоить данная работа?", 12000);
+console.log(screenPrice);
+console.log(typeof screenPrice);
+
+adaptive = confirm("Нужен ли адаптив на сайте?");
+console.log(adaptive);
+console.log(typeof adaptive);
+
+let service1 = confirm("Нужна ли дополнительная услуга 1?");
+console.log(service1);
+console.log(typeof service1);
+
+let servicePrice1 = +prompt("Сколько будет стоить данная услуга?", 1000);
+console.log(servicePrice1);
+console.log(typeof servicePrice1);
+
+let service2 = confirm("Нужна ли дополнительная услуга 2?");
+console.log(service2);
+console.log(typeof service1);
+
+let servicePrice2 = +prompt("Сколько будет стоить данная услуга?", 3000);
+console.log(servicePrice2);
+console.log(typeof servicePrice2);
+
+fullPrice = screenPrice + service1*servicePrice1 + service2*servicePrice2;
+console.log(fullPrice);
+
+let servicePercentPrice = Math.ceil(fullPrice - rollback);
+console.log(servicePercentPrice);
+
+switch (true) {
+    case fullPrice >= 30000:
+        console.log ("Даем скидку в 10%");
+        break
+    case fullPrice >= 15000 && fullPrice < 30000:
+        console.log ("Даем скидку в 5%");
+        break
+    case fullPrice >= 0 && fullPrice < 15000:
+        console.log ("Скидка не предусмотрена");
+        break
+    default:
+        console.log ("Что то пошло не так");    
+}
+
